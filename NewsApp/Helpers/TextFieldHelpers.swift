23dde5c03812386a7946497091f3cd6ba58654f3
Evgenii Mikhailov <e.mikhailov@outlook.com>
2023-02-17 17:49:42 +0300

    Fix

---
+++ NewsApp/Helpers/TextFieldHelpers.swift
@@ -0,0 +1,21 @@
+//
+//  TextFieldHelpers.swift
+//  NewsApp
+//
+//  Created by Evgenii Mikhailov on 16.02.2023.
+//
+
+import UIKit
+
+class LeftPaddedTextField: UITextField {
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return CGRect(x: bounds.origin.x + 10, y: bounds.origin.y, width: bounds.width, height: bounds.height)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return CGRect(x: bounds.origin.x + 10, y: bounds.origin.y, width: bounds.width, height: bounds.height)
+    }
+}
+
+
